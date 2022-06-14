a85a5032a7029f92aeb1ee6a9fceaa08a828492c
余信和 <asd23353934@gmail.com>
2022-06-14 21:33:01 +0800

    text

--- models/Sql.php
+++ models/Sql.php
@@ -23,30 +23,26 @@ class Sql
     public function movie_ticket()
     {
         $sql = "SELECT movie_ticket.ticket_id,
-                       screenings.screen_id,
+                       movie_ticket.foodAll,
+                       movie_ticket.priceAll,
                        screenings.show_date,
                        screenings.show_time,
-                       screenings.price,
                        movie_ticket.seat_name,
-                       foods.name AS foodsName,
-                       foods.price,
-                       theaters.name AS theatersName,
-                       cinemas.name AS cinemasName,
-                       categorys.name AS categorysName,
-                       movies.name AS moviesName,
+                       theaters.name as theatersName,
+                       cinemas.name as cinemasName,
+                       categorys.name as categorysName,
+                       movies.name as moviesName,
                        movies.type              
-                       FROM screenings
-                       JOIN movie_ticket ON screenings.screen_id = movie_ticket.screen_id
-                       JOIN foodall ON movie_ticket.ticket_id = foodall.ticket_id
-                       JOIN foods ON foodall.food_id = foods.food_id
-                       JOIN theaters ON theaters.theater_id = screenings.theater_id
-                       JOIN cinemas ON cinemas.cinema_id = theaters.cinema_id
-                       JOIN categorysall ON categorysall.categorysAll_id = screenings.categorysAll_id
-                       JOIN categorys ON categorysall.category_id = categorys.category_id
-                       JOIN movies ON movies.movie_id = categorysall.movie_id
-                       JOIN users ON users.user_id = movie_ticket.user_id
-                       WHERE movie_ticket.user_id = 1
-                       order by movie_ticket.ticket_id desc;";
+                FROM screenings
+                JOIN movie_ticket ON screenings.screen_id = movie_ticket.screen_id
+                JOIN theaters ON theaters.theater_id = screenings.theater_id
+                JOIN cinemas ON cinemas.cinema_id = theaters.cinema_id
+                JOIN categorysall ON categorysall.categorysAll_id = screenings.categorysAll_id
+                JOIN categorys ON categorysall.category_id = categorys.category_id
+                JOIN movies ON movies.movie_id = categorysall.movie_id
+                JOIN users ON users.user_id = movie_ticket.user_id
+                WHERE movie_ticket.user_id = 1
+                order by movie_ticket.ticket_id desc;";
         $result = mysqli_query($this->link, $sql);
         while ($row = mysqli_fetch_assoc($result)) {
             $rows[] = $row;
